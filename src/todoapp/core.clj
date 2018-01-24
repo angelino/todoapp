669712fbe492ca22f03738305194bc25bc6da282
Lucas Angelino <lucas.angelino@gmail.com>
2018-01-24 21:04:21 -0200

    Initial commit

---
+++ src/todoapp/core.clj
@@ -0,0 +1,6 @@
+(ns todoapp.core
+  (:require [ring.adapter.jetty :as jetty]))
+
+(defn -main [port]
+  (jetty/run-jetty (fn [req] {:status 200 :body "Hello World!" :headers {}})
+                   {:port (Integer. port)}))
