2ead03b9d76ca9e2a792277f9601de83770554ab
Lucas Angelino dos Santos <lucas.santos@synchro.com.br>
2018-01-25 07:58:40 -0200

    Use compojure to create routes

--- src/todoapp/core.clj
+++ src/todoapp/core.clj
@@ -1,24 +1,20 @@
 (ns todoapp.core
   (:require [ring.adapter.jetty :as jetty]
-            [ring.middleware.reload :refer [wrap-reload]]))
+            [ring.middleware.reload :refer [wrap-reload]]
+            [compojure.core :refer [defroutes GET]]
+            [compojure.route :refer [not-found]]))
 
-(defn greet [req] 
-  (cond 
-    (= (:uri req) "/")
-    {:status 200
+(defn greet [req]
+  {:status 200
      :body "Hello World!!!!!!"
-     :headers {}}
-    (= (:uri req) "/goodbye") 
-    {:status 200
-     :body "Goodbye, Cruel World!"
-     :headers {}}
-    :else
-    {:status 404
-     :body "Not found!"
-     :headers {}}))
+     :headers {}})
+
+(defroutes app
+  (GET "/" [] greet)
+  (not-found "Page not found"))
 
 (defn -main [port]
-  (jetty/run-jetty greet {:port (Integer. port)}))
+  (jetty/run-jetty app {:port (Integer. port)}))
 
 (defn -dev-main [port]
-  (jetty/run-jetty (wrap-reload #'greet) {:port (Integer. port)}))
+  (jetty/run-jetty (wrap-reload #'app) {:port (Integer. port)}))
