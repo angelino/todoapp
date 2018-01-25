12adc2341f6bc2a163a5687515c166d043c2989c
Lucas Angelino <lucas.angelino@gmail.com>
2018-01-24 22:29:33 -0200

    Exercice 4 - Add compojure lib to the project

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [ring "1.6.3"]]
+                 [ring "1.6.3"]
+                 [compojure "1.6.0"]]
   :main todoapp.core
   :profiles {:dev {:main todoapp.core/-dev-main}})
