ce2b03526244f8a413410a89df4b42444a05322b
Lucas Angelino dos Santos <lucas.angelino@gmail.com>
2018-02-09 21:11:44 -0200

    Remove style included to debug

--- src/todoapp/core.clj
+++ src/todoapp/core.clj
@@ -74,7 +74,6 @@
           [:title "Todo App"]
           [:meta {:name :viewport
                   :content "width=device-width, initial-scale=1.0"}]
-          [:style "* { border: 1px red solid !important; }"]
           [:link {:href "/bootstrap/css/bootstrap.min.css"
                   :rel :stylesheet}]
           [:link {:href "/css/signin.css"
